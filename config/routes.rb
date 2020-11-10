6da14fadc2c62b3262bdaa12e558a0cfb76654e7
Remi Wallaere <r.wallaere@gmail.com>
2020-11-10 14:57:22 +0100

    my task manager without refacto and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_form
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy
 end
